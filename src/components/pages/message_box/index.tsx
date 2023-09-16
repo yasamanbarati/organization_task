11b202d569b34b2ee92fdefee347ed3ef658a3ee
yasaman <yasamanbarati81@gmail.com>
2023-09-16 19:10:46 +0330

    Improvment code&style structrue(chat section,background)

--- src/components/pages/message_box/index.tsx
+++ src/components/pages/message_box/index.tsx
@@ -1,6 +1,31 @@
+import { styled } from "@mui/material"
+import { MessageCard } from "@/components/card/message_card"
+import { AddMessage } from "./add_message"
+import { useState } from "react"
+
+const MessageBox = styled('div')(({ theme }) => ({
+  backgroundImage: `url("/image.svg"), linear-gradient(${theme.palette.violet.dark}, ${theme.palette.dark})`,
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  height: '90%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+}))
 
 export const MessageBoxSection = () => {
+
+  const [message, setMessage] = useState<string[]>();
+
+  const addMassage = (text: string) => {
+    // setMessage([...message, text]);
+  };
+
   return (
-    <div>MessageBoxSection</div>
+    <MessageBox>
+      <MessageCard />
+      <AddMessage />
+    </MessageBox>
   )
 }
