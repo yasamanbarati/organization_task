13a53617e1120c9f7a90f459009cd6387c068195
yasaman <yasamanbarati81@gmail.com>
2023-09-18 22:32:31 +0330

    Improvement code & style structrue(add message)

--- src/components/pages/message_box/add_message/index.tsx
+++ src/components/pages/message_box/add_message/index.tsx
@@ -1,5 +1,5 @@
-import { AttachmentOutlined, CircleOutlined, KeyboardVoiceOutlined } from "@mui/icons-material";
-import { IconButton, InputBase, Paper, styled } from "@mui/material"
+import { AttachmentOutlined, CircleOutlined, KeyboardVoiceOutlined, Send } from "@mui/icons-material";
+import { Button, IconButton, InputBase, Paper, styled } from "@mui/material"
 import { useState } from "react";
 
 interface AddMessageProps {
@@ -7,8 +7,6 @@ interface AddMessageProps {
 }
 
 const FormBox = styled(Paper)(({ theme }) => ({
-    position: "absolute",
-    bottom: 0,
     height: '65px',
     background: theme.palette.violet.dark,
     display: 'flex',
@@ -40,29 +38,50 @@ const InputBaseBox = styled(InputBase)(({ theme }) => ({
     }
 }))
 
-export const AddMessage = () => {
+
+
+export const AddMessage = ({ addMessage }: AddMessageProps) => {
+
 
     const [value, setvalue] = useState("")
 
-    const handelSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+    const handelOnClick = () => {
         if (!value) return;
+
+        console.log({ value });
+
+        addMessage(value);
         setvalue("");
     };
 
     return (
-        <FormBox>
-            <IconButton aria-label="microphone">
-                <KeyboardVoiceOutlined />
-            </IconButton>
-            <IconButton type="button" aria-label="assets" sx={{ transform: "rotateZ(124deg)" }}>
-                <AttachmentOutlined />
-            </IconButton>
+        <FormBox >
+            {value.trim().length > 0 ?
+                <Button
+                    variant="contained"
+                    endIcon={<Send />}
+                    onClick={handelOnClick}
+                    sx={{ background: 'transparent', "&:hover": { background: 'transparent' } }}
+                    disableElevation={true}
+                />
+
+                : <>
+                    <IconButton aria-label="microphone">
+                        <KeyboardVoiceOutlined />
+                    </IconButton>
+                    <IconButton type="button" aria-label="assets" sx={{ transform: "rotateZ(124deg)" }}>
+                        <AttachmentOutlined />
+                    </IconButton>
+                </>}
+
             <InputBaseBox
                 sx={{ ml: 1, flex: 1 }}
                 placeholder="پیام..."
-                inputProps={{ 'aria-label': 'send message' }}
+                autoComplete="off"
+                value={value}
+                onChange={e => setvalue(e.target.value)}
             />
+
             <IconButton aria-label="emoji">
                 <CircleOutlined />
             </IconButton>
