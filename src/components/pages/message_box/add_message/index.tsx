2d1582a52005d5d9ea2322bcdf816ea522409ffb
yasaman <yasamanbarati81@gmail.com>
2023-09-20 00:38:50 +0330

    Improvement code structure(message box)

--- src/components/pages/message_box/add_message/index.tsx
+++ src/components/pages/message_box/add_message/index.tsx
@@ -1,90 +1,103 @@
-import { AttachmentOutlined, CircleOutlined, KeyboardVoiceOutlined, Send } from "@mui/icons-material";
+import {
+  AttachmentOutlined,
+  CircleOutlined,
+  KeyboardVoiceOutlined,
+  Send,
+} from "@mui/icons-material"
 import { Button, IconButton, InputBase, Paper, styled } from "@mui/material"
-import { useState } from "react";
+import { useState } from "react"
 
 interface AddMessageProps {
-    addMessage: (value: string) => void
+  addMessage: (value: string) => void
 }
 
 const FormBox = styled(Paper)(({ theme }) => ({
-    height: '65px',
-    background: theme.palette.violet.dark,
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: '12px',
-    width: '100%',
-    "& .MuiButtonBase-root": {
-        color: theme.palette.white,
-        "& .MuiSvgIcon-root": {
-            fontSize: '1.4rem',
-        }
-    }
+  height: "58px",
+  background: theme.palette.violet.dark,
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  padding: "12px",
+  width: "100%",
+  "& .MuiButtonBase-root": {
+    color: theme.palette.white,
+    "& .MuiSvgIcon-root": {
+      fontSize: "1.4rem",
+    },
+  },
 }))
 const InputBaseBox = styled(InputBase)(({ theme }) => ({
-    background: theme.palette.brightestViolet,
-    padding: '6px 12px',
-    borderRadius: '6px',
-    "& input": {
-        color: theme.palette.white,
-        fontSize: '0.87rem',
-        fontWeight: '500',
-        lineHeight: '15px',
-        letterSpacing: '0',
-        '& ::placeholder': {
-            textOverflow: 'ellipsis !important',
-            color: theme.palette.white,
-        }
-    }
+  background: theme.palette.brightestViolet,
+  padding: "6px 12px",
+  borderRadius: "6px",
+  "& input": {
+    color: theme.palette.white,
+    fontSize: "0.87rem",
+    fontWeight: "500",
+    lineHeight: "15px",
+    letterSpacing: "0",
+    "& ::placeholder": {
+      textOverflow: "ellipsis !important",
+      color: theme.palette.white,
+    },
+  },
 }))
 
-
-
 export const AddMessage = ({ addMessage }: AddMessageProps) => {
+  const [value, setvalue] = useState("")
 
+  const handelOnClick = () => {
+    if (!value) return
 
-    const [value, setvalue] = useState("")
+    addMessage(value)
+    setvalue("")
+  }
 
-    const handelOnClick = () => {
-        if (!value) return;
+  const handleKeyPress = (event: { key: string }) => {
+    if (event.key === 'Enter') {
+      handelOnClick()
+    }
+  }
 
-        console.log({ value });
+  return (
+    <FormBox>
+      {value.trim().length > 0 ? (
+        <Button
+          variant="contained"
+          endIcon={<Send />}
+          onClick={handelOnClick}
+          sx={{
+            background: "transparent",
+            "&:hover": { background: "transparent" },
+          }}
+          disableElevation={true}
+        />
+      ) : (
+        <>
+          <IconButton aria-label="microphone">
+            <KeyboardVoiceOutlined />
+          </IconButton>
+          <IconButton
+            type="button"
+            aria-label="assets"
+            sx={{ transform: "rotateZ(124deg)" }}>
+            <AttachmentOutlined />
+          </IconButton>
+        </>
+      )}
 
-        addMessage(value);
-        setvalue("");
-    };
+      <InputBaseBox
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="پیام..."
+        autoComplete="off"
+        value={value}
+        onChange={(e) => setvalue(e.target.value)}
+        onKeyPress={handleKeyPress}
+      />
 
-    return (
-        <FormBox >
-            {value.trim().length > 0 ?
-                <Button
-                    variant="contained"
-                    endIcon={<Send />}
-                    onClick={handelOnClick}
-                    sx={{ background: 'transparent', "&:hover": { background: 'transparent' } }}
-                    disableElevation={true}
-                />
-
-                : <>
-                    <IconButton aria-label="microphone">
-                        <KeyboardVoiceOutlined />
-                    </IconButton>
-                    <IconButton type="button" aria-label="assets" sx={{ transform: "rotateZ(124deg)" }}>
-                        <AttachmentOutlined />
-                    </IconButton>
-                </>}
-
-            <InputBaseBox
-                sx={{ ml: 1, flex: 1 }}
-                placeholder="پیام..."
-                autoComplete="off"
-                value={value}
-                onChange={e => setvalue(e.target.value)}
-            />
-
-            <IconButton aria-label="emoji">
-                <CircleOutlined />
-            </IconButton>
-        </FormBox>
-    )
+      <IconButton aria-label="emoji">
+        <CircleOutlined />
+      </IconButton>
+    </FormBox>
+  )
 }
--- src/components/pages/message_box/index.tsx
+++ src/components/pages/message_box/index.tsx
@@ -1,49 +1,57 @@
-import { styled } from '@mui/material'
-import { MessageCard } from '@/components/card/message_card'
-import { AddMessage } from './add_message'
-import { useEffect, useRef, useState } from 'react'
-import { ScrollButton } from './scroll_button'
-import { useRouter } from 'next/router'
-import { messageProps, messagePropsEmpty } from '@/services/type'
-import { useAppSelector } from '@/setup/redux/react-hooks'
+import { styled } from "@mui/material"
+import { MessageCard } from "@/components/card/message_card"
+import { AddMessage } from "./add_message"
+import { useEffect, useRef, useState } from "react"
+import { ScrollButton } from "./scroll_button"
+import { useRouter } from "next/router"
+import { messageProps } from "@/services/type"
+import { toast, ToastContainer } from 'react-toastify';
 
-const MessageBox = styled('div')(({ theme }) => ({
-  position: 'relative',
+import "react-toastify/dist/ReactToastify.css";
+
+const MessageBox = styled("div")(({ theme }) => ({
+  position: "relative",
   backgroundImage: `url("/image.svg"), linear-gradient(${theme.palette.violet.dark}, ${theme.palette.dark})`,
-  backgroundRepeat: 'no-repeat',
-  backgroundSize: 'cover',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  width: '100%',
-  maxWidth: '400px',
-  padding: '0 12px 12px',
-  height: 'calc(100% - 121px)',
-  overflow: 'auto',
-  '&::-webkit-scrollbar': {
-    width: '2px',
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  width: "100%",
+  maxWidth: "400px",
+  padding: "0 12px 12px",
+  height: "calc(100% - 121px)",
+  overflow: "auto",
+  "&::-webkit-scrollbar": {
+    width: "2px",
   },
 
-  '&::-webkit-scrollbar-thumb': {
-    backgroundColor: '#CCCCCC',
-    borderRadius: '20px',
+  "&::-webkit-scrollbar-thumb": {
+    backgroundColor: "#CCCCCC",
+    borderRadius: "20px",
   },
 }))
+const ToastContainerBox = styled(ToastContainer)(({ theme }) => ({
+  top: '0!important',
+  right: '0!important',
+  width: '260px!important',
+}))
+
 
 const messageData: messageProps = {
-  text: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم استلورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است.',
+  text: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم استلورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است.",
   visibilityCount: 1,
   favoriteCount: 49,
-  time: new Date('December 17, 2023 03:24:00 GMT+0330 (Iran Standard Time)'),
+  time: new Date("December 19, 2023 16:24:00 GMT+0330 (Iran Standard Time)"),
 }
 
 export const MessageBoxSection = () => {
   const router = useRouter()
-  console.log(router.query.slug)
   const value = router.query.slug
 
   const [message, setMessage] = useState<messageProps[]>([messageData])
+
+  const [isMark, setIsMark] = useState(false)
   const boxRef = useRef<any>(null)
 
   const addMassage = (text: string) => {
@@ -53,7 +61,7 @@ export const MessageBoxSection = () => {
         text,
         visibilityCount: 1,
         favoriteCount: 0,
-        time: new Date()
+        time: new Date(),
       },
     ]
     localStorage.setItem(`chatList${value}`, JSON.stringify(newMessage))
@@ -73,10 +81,28 @@ export const MessageBoxSection = () => {
     boxRef?.current?.scrollTo(0, boxRef.current?.scrollHeight)
   }, [message])
 
+  const notify = () => {
+    setIsMark(!isMark)
+    if (isMark) return
+    else {
+      toast.success('پیام مورد نظر سیو شد', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    }
+
+  }
   return (
     <>
+      <ToastContainerBox style={{ position: 'absolute' }} />
+
       <MessageBox ref={boxRef}>
-        <ScrollButton useRef={boxRef} />
         {message.map((item, index) => {
           return (
             <MessageCard
@@ -85,10 +111,12 @@ export const MessageBoxSection = () => {
               favoriteCount={item.favoriteCount}
               visibilityCount={item.visibilityCount}
               time={item.time}
+              saveMessageFUN={notify}
             />
           )
         })}
       </MessageBox>
+      <ScrollButton useRef={boxRef} />
 
       <AddMessage addMessage={addMassage} />
     </>
