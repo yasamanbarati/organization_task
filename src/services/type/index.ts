7707bc3766a1414ddfacd978beba95cb878bb1b3
yasaman <yasamanbarati81@gmail.com>
2023-09-20 00:36:17 +0330

    Development server services structure

--- src/services/type/index.ts
+++ src/services/type/index.ts
@@ -1,12 +1,23 @@
+import { ReactNode } from 'react'
+
 export interface messageProps {
   text: string
   visibilityCount: number
   favoriteCount: number
   time: Date
+  saveMessageFUN?: () => void
+}
+export interface TabCardProps {
+  value?: number
+  avatar: string
+  title: string
+  messageCount?: number
+  time?: Date
+  subheader?: string & ReactNode
 }
 export const messagePropsEmpty = {
   text: '',
   visibilityCount: 1,
   favoriteCount: 0,
-  time: '',
+  time: '2023-12-16T23:54:00.000Z',
 }
